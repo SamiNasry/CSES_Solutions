93ac08fdfdc2bd9dac1414fc064c5eb616a18096
Sami Nasry <skelowthes@gmail.com>
2024-11-29 17:37:58 +0100

    Creating Strings done

--- Introductory_Problems/15.Creating_Strings.cpp
+++ Introductory_Problems/15.Creating_Strings.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
+#include <set>
 #include <string>
-
-
 
 using namespace std;
 
-typedef long long ll;
+void Permutation(string &s, int index, set<string> &resultat)
+{
+	if (index == s.size())
+	{
+		resultat.insert(s);
+		return;
+	}
 
+	for(int i = index; i < s.size(); i++)
+	{
+		swap(s[index], s[i]);
+		Permutation(s, index + 1, resultat);
+		swap(s[index], s[i]);
+	}
 
+}
 
 int main()
 {
-		string x;
-		ll n{0};
-		getline(cin, x);
-		for(char i : x)
-		{
-			n++;
-		}
-		cout << n*(n-1);
+	string input;
+	cin >> input;
+
+	set<string> final;
+
+	Permutation(input, 0, final);
+
+	cout << final.size() << endl;
+
+	for (const string s : final) {
+		cout << s << endl;
+	}
+
 }
